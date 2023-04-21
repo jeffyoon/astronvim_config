9fb8cb296b8ec95443f313bc304750e3e4db4f11
Jeff Yoon <jeyoon@outlook.com>
2023-04-21 13:24:38 +0900

    Update nvim config

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,23 @@
 return {
+  -- Clang Formatting
+  "rhysd/vim-clang-format",
+  "ericcurtin/CurtineIncSw.vim",
+  -- Vim-tmux navigator
+  "christoomey/vim-tmux-navigator",
+  -- Fuzzy finder
+  { "junegunn/fzf",     dir = "~/.fzf", build = "./install --all", lazy = false },
+  { "junegunn/fzf.vim", lazy = false },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+  { "mxsdev/nvim-dap-vscode-js" },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
